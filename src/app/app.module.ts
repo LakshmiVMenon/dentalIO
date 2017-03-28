0b6d15acfdb236e953c54efa0f8fe67766b1f44f
lakshmimenon <lakshmimenon@qburst.com>
2017-03-28 11:44:14 +0530

    Enabling Production mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {enableProdMode} from '@angular/core';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login.component';
@@ -11,6 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoginService } from './login.service';
 import { DocDetailService } from './docdetail.service';
 
+enableProdMode();
 @NgModule({
   declarations: [
     AppComponent,
